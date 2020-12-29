0543942b2f6bbe300ba9b2f77a0ed9b85bdfcd35
Kevin Ou <therealkevinou@yahoo.com>
2020-12-29 15:45:26 -0600

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2',
+  },
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  mode: 'production',
+};
