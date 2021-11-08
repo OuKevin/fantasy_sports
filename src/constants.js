56363fcac22537ee5968b3069517690350719d27
Kevin Ou <therealkevinou@yahoo.com>
2021-11-08 08:16:43 -0600

    fix: set streak thrshold to 3

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ export const FANTASY_V3_BASE_ENDPOINT = 'https://fantasy.espn.com/apis/v3/games'
 export const TIE = 'TIE';
 export const LOSE = 'LOSE';
 export const WIN = 'WIN';
-export const STREAK_THRESHOLD = 2;
+export const STREAK_THRESHOLD = 3;
 // TODO: add more as needed
 export const CATEGORY_TO_STAT_INDEX = {
   fba: {
